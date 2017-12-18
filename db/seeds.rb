c5c24421103df72c0b81b466a1d429e746045da0
AnthonyChamberlin <antchamberlin.js@gmail.com>
2017-12-18 16:44:14 +0000

    Add Carrierwave and Cloudinary Gem and generate image uploader file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,51 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 User.destroy_all
 Track.destroy_all
 Genre.destroy_all
 Type.destroy_all
 Review.destroy_all
+
+# --- T Y P E
+
+sample = Type.create!(name: "Sample")
+full = Type.create!(name: "Full Track")
+
+
+# --- T R A C K S
+
+
+
+
+
+# --- G E N R E
+
+acoustic = Genre.create!(name:"Acoustic")
+blues = Genre.create!(name:"Blues")
+rnb = Genre.create!(name:"RnB")
+rock = Genre.create!(name:"Rock")
+punk = Genre.create!(name:"Punk")
+metal = Genre.create!(name:"Metal")
+jazz = Genre.create!(name:"Jazz")
+reggae = Genre.create!(name:"Reggae")
+dub = Genre.create!(name:"DUB")
+house = Genre.create!(name:"House")
+techno = Genre.create!(name:"Techno")
+dnb = Genre.create!(name:"Drum N Bass")
+ambient = Genre.create!(name:"Ambient")
+inst = Genre.create!(name:"Intsrumental")
+theme = Genre.create!(name:"Theme")
+pop = Genre.create!(name:"Pop")
+hiphop = Genre.create!(name:"Hip Hop")
+folk = Genre.create!(name:"Folk")
+country = Genre.create!(name:"Country")
+classical = Genre.create!(name:"Classical")
+funk = Genre.create!(name:"Funk")
+rap = Genre.create!(name:"Rap")
+dance = Genre.create!(name:"Dance")
+soul = Genre.create!(name:"Soul")
+dupstep = Genre.create!(name:"Dubstep")
+
+
+# --- U S E R S
 
 john_mayer = User.create!(first_name: "John", last_name: "Mayer", username: "John Mayer", password: "123456", email:"john@samplrfy.com")
 ed_sheeran = User.create!(first_name: "Ed", last_name: "Sheeran", username: "Ed Sheeran", password: "123456", email:"ed@samplrfy.com")
@@ -21,38 +57,3 @@ fat_freddy = User.create!(first_name: "Fat", last_name: "Freddies Drop", usernam
 brian_eno = User.create!(first_name: "Brian", last_name: "Eno", username: "Brian Eno", password: "123456", email:"brian@samplrfy.com")
 otis_redding = User.create!(first_name: "Otis", last_name: "Redding", username: "Otis Redding", password: "123456", email:"otis@samplrfy.com")
 netsky = User.create!(first_name: "Netsky", last_name: "", username: "Netsky", password: "123456", email:"netsky@samplrfy.com")
-
-
-Genre.create!([
-    {name: "Acoustic"},
-    {name: "Blues"},
-    {name: "RnB"},
-    {name: "Rock"},
-    {name: "Punk"},
-    {name: "Metal"},
-    {name: "Jazz"},
-    {name: "Reggae"},
-    {name: "DUB"},
-    {name: "House"},
-    {name: "Techno"},
-    {name: "Drum N Bass"},
-    {name: "Ambient"},
-    {name: "Instrumental"},
-    {name: "Theme"},
-    {name: "Pop"},
-    {name: "Hip Hop"},
-    {name: "Folk"},
-    {name: "Country"},
-    {name: "Classical"},
-    {name: "Funk"},
-    {name: "Rap"},
-    {name: "Dance"},
-    {name: "Soul"},
-    {name: "Dubstep"},
-  ])
-
-
-  Type.create!([
-    {name: "Sample"},
-    {name: "Full Track"}
-    ])
