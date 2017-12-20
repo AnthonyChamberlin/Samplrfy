9347f0fa4a411201c6210077074355f98a234485
AnthonyChamberlin <antchamberlin.js@gmail.com>
2017-12-20 16:11:37 +0000

    Migrate and Ensure Status Up for Postgresql

--- db/migrate/20170926144334_remove_columns_from_tracks.rb
+++
@@ -1,8 +0,0 @@
-class RemoveColumnsFromTracks < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :tracks, :audio_file_name, :string
-    remove_column :tracks, :audio_content_type, :string
-    remove_column :tracks, :audio_file_size, :integer
-    remove_column :tracks, :audio_updated_at, :datetime
-  end
-end
